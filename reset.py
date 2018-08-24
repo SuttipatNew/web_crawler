e86beee2b2556b16971694268db09c23f91dfa57
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2018-08-24 23:37:12 +0700

    added http://www.eng.ku.ac.th to seed

--- reset.py
+++ reset.py
@@ -1,4 +1,5 @@
 import os, codecs
 os.system('rm visited_q count list_robots.txt list_sitemap.txt')
 with codecs.open('./frontier_q', 'w', 'utf-8') as f:
-    f.write('http://www.ku.ac.th/web2012/')
+    f.write('http://www.ku.ac.th/web2012/\n')
+    f.write('http://www.eng.ku.ac.th/')
